5fbf1ef65f6bab8b4c5859dbac12e339db14cb62
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-04-24 19:04:56 +0100

    Create validar_password

--- validate.py
+++ validate.py
@@ -8,3 +8,14 @@ def validar_email(email):
         return True
     return False
 
+# validação de senha utilizando regex (senha deve conter pelo menos 8 caracteres, pelo menos 1 letra maiúscula, e pelo menos 1 número)
+
+def validar_password(password):
+    if len(password) < 8:
+        return False
+    if not re.search(r"[A-Z]", password):  
+        return False
+    if not re.search(r"[0-9]", password):
+        return False
+    return True
+
