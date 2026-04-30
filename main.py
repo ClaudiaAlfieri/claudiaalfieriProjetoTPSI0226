6cd458c2e285f60b6044fce97d05bc97df4a7a67
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-04-30 15:06:42 +0100

    Add imports to main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from data import carregar_filmes, guardar_filmes
+from validate import validar_email, validar_password, validar_data
+from data import carregar_filmes, guardar_filmes, carregar_config
+
+
