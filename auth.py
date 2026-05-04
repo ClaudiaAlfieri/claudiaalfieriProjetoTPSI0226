9c927079f38026e64a264493246d81138a2ad70c
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-04 15:28:58 +0100

    Move def login from main.py to auth.py and correct imports

--- auth.py
+++ auth.py
@@ -1,2 +1,25 @@
 from data import carregar_config
 from validate import validar_email, validar_password
+
+#Login
+def login():
+    config = carregar_config()
+    while True:
+        while True:
+            email = input("Digite seu email: ")
+            if validar_email(email):
+                break  
+            else:
+                print("Email inválido. Tente novamente.")
+        
+        while True:
+            password = input("Digite sua password: ")
+            if validar_password(password):
+                break  
+            else:
+                print("Password inválido. Tente novamente.")
+
+        if email == config["email"] and password == config["pass"]:
+            return True
+        else:
+            print("Email ou password incorretos. Tente novamente.")
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from data import carregar_filmes, guardar_filmes, carregar_config
-from validate import validar_email, validar_password, validar_data
-
-
-
+from data import carregar_filmes, guardar_filmes
+from auth import login
 
