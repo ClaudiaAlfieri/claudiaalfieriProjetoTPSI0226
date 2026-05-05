bd8cf023b61f79a0d9b60c7cf08a0dfb5412109d
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-05 11:30:20 +0100

    Create loop menu in main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from data import carregar_filmes, guardar_filmes
 from auth import login
+from menus import mostrar_menu
 
 filmes = []
 
 if login():
     filmes = carregar_filmes()
-       
+
+
+while True:
+    opcao = mostrar_menu()
+    if opcao == "1":
+        ver_filmes()
+    elif opcao == "2":
+        adicionar_filme()
+    elif opcao == "3":
+        editar_filme()
+    elif opcao == "4":
+        apagar_filme()
+    elif opcao == "5":
+        pesquisar_filme()
+    elif opcao == "6":
+        guardar_dados()
+    elif opcao == "7":
+        salvar = input("Deseja guardar (S/N)? ")
+        if salvar == "S":
+            guardar_filmes(filmes)
+            break
+        else:
+            break
