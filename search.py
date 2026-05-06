74396e8c3bef943aaafaf05405a13d9598dfaf5f
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-06 23:44:55 +0100

    Create def pesquisa_binaria in search

--- search.py
+++ search.py
@@ -6,3 +6,19 @@ def pesquisa_linear(filmes, termo):
         if termo.lower() in filme["titulo"].lower():
             resultados.append(filme)
     return resultados
+
+
+#Função par pesquisa_binaria()
+
+def pesquisa_binaria(filmes, ano):
+    esquerda = 0
+    direita = len(filmes) - 1
+    while (esquerda <= direita):
+        meio = (esquerda + direita) // 2
+        if filmes[meio]["ano"] == ano:
+            return [filmes[meio]]
+        elif filmes[meio]["ano"] < ano:
+            esquerda = meio + 1
+        elif filmes[meio]["ano"] > ano:
+            direita = meio - 1
+    return []
