ec37aa5661e6246e720db62fd7d8b47dedba3b0a
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-07 21:53:45 +0100

    Create def selection_sort in sort.py

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-#Função que faz o Bubble Sort que ordena comparando dois elementos adjacentes da lista e fazendo a troca deles.
+#Função que faz o Bubble Sort que ordena comparando dois elementos vizinhos na lista e fazendo a troca deles.
 
 def bubble_sort(filmes, campo):
     n = len(filmes)
@@ -7,4 +7,16 @@ def bubble_sort(filmes, campo):
             if filmes[j][campo] > filmes[j+1][campo]:
                 # troca os dois elementos
                 filmes[j], filmes[j+1] = filmes[j+1], filmes[j]
+    return filmes
+
+#Função Selection Sort que ordena procurando o menor elementa da lista e colocando na primeira posição.
+
+def selection_sort(filmes, campo):
+    n = len(filmes)
+    for i in range(n):
+        menor = i
+        for j in range(i+1, n):
+            if filmes[j][campo] < filmes[menor][campo]:
+                menor = j
+        filmes[i], filmes[menor] = filmes[menor], filmes[i]
     return filmes
