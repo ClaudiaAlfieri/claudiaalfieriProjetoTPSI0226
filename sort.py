6b4d294d1c612ea6683f977f3801318db9573931
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-07 23:10:27 +0100

    Create def ordenar_filmes in sort.py

--- sort.py
+++ sort.py
@@ -1,4 +1,6 @@
-#Função que faz o Bubble Sort que ordena comparando dois elementos vizinhos na lista e fazendo a troca deles.
+from filmes import ver_filmes
+
+#Função que ordena comparando dois elementos vizinhos na lista e fazendo a troca deles.
 
 def bubble_sort(filmes, campo):
     n = len(filmes)
@@ -9,7 +11,7 @@ def bubble_sort(filmes, campo):
                 filmes[j], filmes[j+1] = filmes[j+1], filmes[j]
     return filmes
 
-#Função Selection Sort que ordena procurando o menor elementa da lista e colocando na primeira posição.
+#Função que ordena procurando o menor elementa da lista e colocando na primeira posição.
 
 def selection_sort(filmes, campo):
     n = len(filmes)
@@ -20,3 +22,29 @@ def selection_sort(filmes, campo):
                 menor = j
         filmes[i], filmes[menor] = filmes[menor], filmes[i]
     return filmes
+
+#Função que coordena os dois tipos de ordenação (Bubble Sort e Selection Sort).
+
+def ordenar_filme(filmes):
+    print("Campos disponíveis:")
+    print("1. ano")
+    print("2. nota")
+    print("3. titulo")  
+    campo = input("Escolha o campo: ")
+    if campo == "1":
+        campo = "ano"
+    elif campo == "2":
+        campo = "nota"
+    elif campo == "3":
+        campo = "titulo"
+    ordem = input("Ordem (C-Crescente / D-Decrescente): ")    
+    print("1. Ordenar por Bubble Sort (compara e troca elementos vizinhos)")
+    print("2. Ordenar por Selection Sort (procura o menor e coloca no início)")
+    opcao = input("Escolha: ")       
+    if opcao == "1":
+        resultados = bubble_sort(filmes, campo)
+    elif opcao == "2":
+        resultados = selection_sort(filmes, campo)
+    if ordem == "D":
+        resultados.reverse()
+    ver_filmes(resultados)
