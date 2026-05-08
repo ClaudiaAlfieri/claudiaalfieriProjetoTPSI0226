54cca7e5990dd761088bbb8ed40c665f603b9aa3
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-08 18:01:40 +0100

    Create def nota_maxima_minima in estatisticas.py

--- estatisticas.py
+++ estatisticas.py
@@ -9,3 +9,18 @@ def media_notas(filmes):
         total += filme["nota"]
     media = total / len(filmes)
     return media
+
+# Função para verificar a nota mais alta e mais baixa
+
+def nota_maxima_minima(filmes):
+    if len(filmes) == 0:
+        print("Nenhum filme registado.")
+        return
+    maior = filmes[0]
+    menor = filmes[0]
+    for filme in filmes:
+        if filme["nota"] > maior["nota"]:
+            maior = filme
+        if filme["nota"] < menor["nota"]:
+            menor = filme
+    return maior, menor
