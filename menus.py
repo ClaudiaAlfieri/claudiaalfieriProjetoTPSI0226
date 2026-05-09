f42ee2a04f137faf0f70b72526d5f9e4c2a7f763
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-09 23:19:37 +0100

    Fix menus.py

--- menus.py
+++ menus.py
@@ -1,6 +1,7 @@
 #Função para mostrar o menu no ecrã e pedir ao utilizador para escolher uma opção.
 
 def mostrar_menu():   
+    print()
     print("========== PyFlix ==========")
     print("1. Ver lista de filmes")
     print("2. Adicionar filme")
