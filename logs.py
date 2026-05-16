f0d14c3fcd23a64f16f46ca4e17c2b018c993abd
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-16 23:07:10 +0100

    Create def ver_logs

--- logs.py
+++ logs.py
@@ -7,3 +7,17 @@ def registar_log(mensagem):
     agora = datetime.now().strftime("%d-%m-%Y %H:%M")
     with open("logs.txt", "a", encoding="utf-8") as ficheiro:
         ficheiro.write(f"[{agora}] {mensagem}\n")
+        
+ 
+#Função para ver todos os logs 
+        
+def ver_logs():
+    try:
+        with open("logs.txt", "r", encoding="utf-8") as ficheiro:
+            conteudo = ficheiro.read()
+            if conteudo:
+                print(conteudo)
+            else:
+                print("Nenhum log registado.")
+    except FileNotFoundError:
+        print("Nenhum log registado.")
